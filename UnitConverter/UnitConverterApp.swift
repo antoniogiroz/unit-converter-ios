9d481b61048e4056f0114d56ca6320fb8d996c49
Antonio Giroz <antonioluisgil@gmail.com>
2025-09-26 16:46:35 +0200

    Initial Commit

---
+++ UnitConverter/UnitConverterApp.swift
@@ -0,0 +1,17 @@
+//
+//  UnitConverterApp.swift
+//  UnitConverter
+//
+//  Created by Antonio Giroz on 26/9/25.
+//
+
+import SwiftUI
+
+@main
+struct UnitConverterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
