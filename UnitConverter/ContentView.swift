ed95bf0314eb6031d6c4930379b5a4f51fbb6508
Antonio Giroz <antonioluisgil@gmail.com>
2025-09-27 13:26:25 +0200

    feat: ✨ implement unit conversion

--- UnitConverter/ContentView.swift
+++ UnitConverter/ContentView.swift
@@ -8,14 +8,88 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var inputUnit: Dimension = UnitLength.meters
+    @State private var outputUnit: Dimension = UnitLength.kilometers
+    @State private var inputValue = 1.0
+    @State private var selectedUnits = 0
+    
+    @FocusState private var inputIsFocused: Bool
+    
+    let conversions = ["Distance", "Mass", "Temperature", "Time"]
+    
+    let unitTypes = [
+        [UnitLength.meters, UnitLength.kilometers, UnitLength.feet, UnitLength.yards, UnitLength.miles],
+        [UnitMass.grams, UnitMass.kilograms, UnitMass.ounces, UnitMass.pounds],
+        [UnitTemperature.celsius, UnitTemperature.fahrenheit, UnitTemperature.kelvin],
+        [UnitDuration.hours, UnitDuration.minutes, UnitDuration.seconds]
+    ]
+    let formatter: MeasurementFormatter
+    
+
+    var result: String {
+        let inputMeasurement = Measurement(value: inputValue, unit: inputUnit)
+        let outputMeasurement = inputMeasurement.converted(to: outputUnit)
+        
+        return formatter.string(from: outputMeasurement)
+    }
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            Form {
+                Section {
+                    Picker("Conversion", selection: $selectedUnits) {
+                        ForEach(0..<conversions.count, id: \.self) {
+                            Text(conversions[$0])
+                        }
+                    }
+                }
+                
+                Section("From unit") {
+                    Picker("Input unit", selection: $inputUnit) {
+                        ForEach(unitTypes[selectedUnits], id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                    
+                    TextField("Enter a value", value: $inputValue, format: .number)
+                        .keyboardType(.decimalPad)
+                        .focused($inputIsFocused)
+                }
+                
+                Section("To unit") {
+                    Picker("Output unit", selection: $outputUnit) {
+                        ForEach(unitTypes[selectedUnits], id: \.self) {
+                            Text($0.symbol)
+                        }
+                    }
+                    .pickerStyle(.segmented)
+                }
+                
+                Section("Result") {
+                    Text(result)
+                }
+            }
+            .navigationTitle("Unit Converter")
+            .toolbar {
+                if inputIsFocused {
+                    Button("Done") {
+                        inputIsFocused = false
+                    }
+                }
+            }
+            .onChange(of: selectedUnits) {
+                let units = unitTypes[selectedUnits]
+                inputUnit = units[0]
+                outputUnit = units[1]
+            }
         }
-        .padding()
+    }
+    
+    init() {
+        formatter = MeasurementFormatter()
+        formatter.unitOptions = .providedUnit
+        formatter.unitStyle = .long
     }
 }
 
